93c373ec38e6637b1722f30038c7201dbd1bf417
云天河 <hlzblog@vip.qq.com>
2021-03-11 01:50:35 +0800

    update

--- driver/xrabbitmq/mq_test.go
+++ driver/xrabbitmq/mq_test.go
@@ -3,10 +3,12 @@ package xrabbitmq
 import (
 	"github.com/HaleyLeoZhang/go-component/driver/xlog"
 	"testing"
+	"time"
 )
 
 var app = &AMQP{}
 var config = &Config{}
+var routineKey = "email.sender"
 
 func TestMain(m *testing.M) {
 	config.Host = "192.168.56.110"
@@ -16,19 +18,26 @@ func TestMain(m *testing.M) {
 	config.Password = "guest"
 	app.Conf = config
 	// 初始化配置
-	app.PullLimit = 3 // 每次最多拉多少条
-	app.ConsumerLimit = 2 // 每次最多 多少个消费者
+	app.PullLimit = 3          // 每次最多拉多少条
+	app.ConsumerLimit = 2      // 每次最多 多少个消费者
 	app.Exchange = "amq.topic" // 交换机名
 	app.Queue = "email_sender" // 消费队列
 	app.Start()
 	app.QueueDeclare()
-	app.BindRoutingKey("email.sender") // 初始化约定要绑定的 routing_key
+	app.BindRoutingKey(routineKey) // 初始化约定要绑定的 routing_key
 	xlog.Infof("RabbitMQ.Init.Exchange (%v) Queue (%v)", app.Exchange, app.Queue)
 	m.Run()
 	app.Close()
 }
 
-
 func TestService_Push(t *testing.T) {
-	app.Push()
+	app.Push(app.Exchange, routineKey, []byte(`{"title":"For test","content":"unit test","sender_name":"local","receiver":"haleyleozhang@sohu.com","receiver_name":""}`))
+}
+
+func TestService_Pull(t *testing.T) {
+	app.Pull(func(bytes []byte) error {
+		<-time.After(1 * time.Second)
+		t.Logf("success")
+		return nil
+	})
 }
